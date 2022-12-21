38fa388d6f66248f643c75e1a512412ffe7d0988
vincentiusdaryl <94355387+vincentiusdaryl@users.noreply.github.com>
2022-12-21 10:06:37 +0700

    Add files via upload

--- loadData.py
+++ loadData.py
@@ -9,14 +9,17 @@ from hdfs import InsecureClient
 client_hdfs = InsecureClient('http://localhost:9870')
 
 df_amazon_apparel_reviews = pd.read_csv('amazon_apparel_reviews.csv')
+df_amazon_automotive_reviews = pd.read_csv('amazon_automotive_reviews.csv')
 df_amazon_beauty_reviews = pd.read_csv('amazon_beauty_reviews.csv')
 df_amazon_books_reviews = pd.read_csv('amazon_books_reviews.csv')
+df_amazon_furniture_reviews = pd.read_csv('amazon_furniture_reviews.csv')
 df_amazon_grocery_reviews = pd.read_csv('amazon_grocery_reviews.csv')
 df_amazon_jewelry_reivews = pd.read_csv('amazon_jewelry_reviews.csv')
 df_amazon_shoes_reviews = pd.read_csv('amazon_shoes_reviews.csv')
 df_amazon_toys_reviews = pd.read_csv('amazon_toys_reviews.csv')
 df_app_reviews = pd.read_csv('app_reviews.csv')
 df_beer_reviews = pd.read_csv('beer_reviews.csv')
+df_drug_reviews = pd.read_csv('drug_reviews.csv')
 
 with client_hdfs.write('/user/data/basdatpbd/dataTubes/amazon_apparel_reviews',encoding='utf-8') as writer:
     df_amazon_apparel_reviews.to_csv(writer, index = False)
